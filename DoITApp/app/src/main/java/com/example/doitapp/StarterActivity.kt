7f681ad762afa1c6c306c699f2007ee607eb28bf
hafid abi d <abi.daniswara@gmail.com>
2022-02-08 01:47:59 +0700

    add file template gan

---
+++ DoITApp/app/src/main/java/com/example/doitapp/StarterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.doitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StarterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
