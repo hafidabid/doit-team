6c374512be1cee17a2e60e5fdf0f5bfbc711bec1
hafid abi d <abi.daniswara@gmail.com>
2022-02-11 19:14:33 +0700

    progress again

--- DoITApp/app/src/main/java/com/example/doitapp/StarterActivity.kt
+++ DoITApp/app/src/main/java/com/example/doitapp/StarterActivity.kt
@@ -1,11 +1,27 @@
 package com.example.doitapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.doitapp.databinding.ActivityMainBinding
+import com.example.doitapp.welcomepage.WelcomePageActivity
+import java.util.*
+
+import kotlin.concurrent.schedule
 
 class StarterActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        Timer("loadingUp Application", false).schedule(200){
+            val intent = Intent(applicationContext, WelcomePageActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
--- DoITApp/app/src/main/java/com/example/doitapp/welcomepage/WelcomePageActivity.kt
+++ DoITApp/app/src/main/java/com/example/doitapp/welcomepage/WelcomePageActivity.kt
@@ -3,10 +3,20 @@ package com.example.doitapp.welcomepage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.doitapp.R
+import com.example.doitapp.databinding.ActivityWelcomePageBinding
 
 class WelcomePageActivity : AppCompatActivity() {
+    lateinit var binding: ActivityWelcomePageBinding
+    lateinit var welcomeScreenFragment: WelcomeScreenFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_page)
+
+        welcomeScreenFragment = WelcomeScreenFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.welcomeFragment, welcomeScreenFragment)
+            commit()
+        }
     }
 }
